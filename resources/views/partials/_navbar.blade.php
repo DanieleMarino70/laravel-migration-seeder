1d127185f5238bbf547c709054e269e8d74627a3
DanieleMarino70 <danielaotitan@gmail.com>
2023-03-30 18:34:33 +0200

    fix: route on web and _navbar

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -2,7 +2,7 @@
     <nav class="d-flex justify-content-center py-3">
       <ul class="nav nav-pills">
         <li class="nav-item"><a href="{{ route('home') }}" class="nav-link @if(Request::route()?->getName() == 'home') active @endif" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="{{ route('list_trains') }}" class="nav-link">Treni</a></li>
+        <li class="nav-item"><a href="{{ route('list_trains') }}" class="nav-link @if(Request::route()?->getName() == 'list_trains') active @endif">Treni</a></li>
       </ul>
     </nav>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'home'])->name('home');
-Route::get('/', [PageController::class, 'list_trains'])->name('list_trains');
+Route::get('/trains', [PageController::class, 'list_trains'])->name('list_trains');
